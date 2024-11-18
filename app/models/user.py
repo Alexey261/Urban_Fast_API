3b1bc6212a52d682a239e0d6f27cc8ca3aa4d2a7
Alexey261 <dlyx@yandex.ru>
2024-11-18 02:52:39 +0200

    Add files via upload

---
+++ app/models/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from app.backend.db import Base
+from app.models import *
+from sqlalchemy.schema import CreateTable
+
+class User(Base):
+    __tablename__ = 'users'
+    __table_args__ = {'keep_existing': True}
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+    tasks = relationship(argument='Task', back_populates='user')
+
+# print(CreateTable(User.__table__))
